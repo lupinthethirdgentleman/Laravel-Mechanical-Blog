0fce5fe7d6e03c6fa1b931ddb878b6ad4262801c
PooH <top.talent65@protonmail.com>
2019-07-29 14:17:44 +0900

    auth added

--- routes/web.php
+++ routes/web.php
@@ -87,3 +87,7 @@ Route::group(['middleware'=>'auth'], function (){
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
